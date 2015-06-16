de04ca97608ae065d03e7275549b0bd8706d053e
llt8248 <15021080@qq.com>
2015-06-16 18:47:06 +0800

    11

---
+++ scripts/teacherwork.js
@@ -0,0 +1,22 @@
+
+
+$(function(){
+	size();
+});
+
+$(window).resize(function(){
+	size();
+});
+
+function size(){
+	var _width = $(window).width(); 
+	if(_width > 1280 && _width < 1600){
+		$(".con_zz div").css("width","20%");
+	}
+	if(_width >= 1600 && _width < 1920){
+	   $(".con_zz div").css("width","16.6%");
+	}
+	if(_width >= 1920){
+	   $(".con_zz div").css("width","14.2%");
+	}
+};
